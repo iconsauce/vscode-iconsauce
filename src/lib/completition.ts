e23ee8b9f3589996ff36abae766d714a74378ea7
Nicola Tamburini <nicola.tamburini@maggioli.it>
2022-03-22 15:47:02 +0100

    feat: add icon preview

--- src/lib/completition.ts
+++ src/lib/completition.ts
@@ -3,8 +3,11 @@ import {
   CompletionItemKind,
   DocumentSelector,
   languages,
+  MarkdownString,
   Position,
+  Range,
   TextDocument,
+  TextEdit,
 } from "vscode";
 import { Log } from "../utils/console";
 import { Extension } from "./extension";
@@ -20,16 +23,29 @@ export class Completition {
 
   registerSelector() {
     return languages.registerCompletionItemProvider(this.documentSelector, {
-      provideCompletionItems: (_document: TextDocument, _position: Position) => {
-        Log.info('build completition');
+      provideCompletionItems: (
+        _document: TextDocument,
+        _position: Position
+      ) => {
         const completitions: CompletionItem[] = [];
         for (const selector of this.extension.getDictionary().keys()) {
           completitions.push(
             new CompletionItem(selector, CompletionItemKind.Unit)
           );
         }
-        Log.info('finish');
         return completitions;
+      },
+      resolveCompletionItem: (item) => {
+        switch (item.kind) {
+          case CompletionItemKind.Unit:
+            item.detail = "iconsauce";
+            const makdownContent = new MarkdownString(`<h1><span style="color:#fff;background-color:#fff;">-<img src="${this.extension.getDictionary().get(item.label.toString())}" width="24px" height="24px"/>-</span></h1>`);
+            makdownContent.supportHtml = true;
+            makdownContent.isTrusted = true;
+            item.documentation = makdownContent;
+            break;
+        }
+        return item;
       },
     });
   }

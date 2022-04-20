aa63061a4b8abe4b85b3fe9dc966d81dfce46a44
Nicola Tamburini <nicola.tamburini@maggioli.it>
2022-04-20 12:40:56 +0200

    fix completition

--- src/lib/completition.ts
+++ src/lib/completition.ts
@@ -1,3 +1,4 @@
+import { match } from "assert";
 import {
   CompletionItem,
   CompletionItemKind,
@@ -22,31 +23,51 @@ export class Completition {
   }
 
   registerSelector() {
-    return languages.registerCompletionItemProvider(this.documentSelector, {
-      provideCompletionItems: (
-        _document: TextDocument,
-        _position: Position
-      ) => {
-        const completitions: CompletionItem[] = [];
-        for (const selector of this.extension.getDictionary().keys()) {
-          completitions.push(
-            new CompletionItem(selector, CompletionItemKind.Unit)
-          );
-        }
-        return completitions;
+    return languages.registerCompletionItemProvider(
+      this.documentSelector,
+      {
+        provideCompletionItems: (
+          document: TextDocument,
+          position: Position
+        ) => {
+          const regexSelector = new RegExp(/([a-zA-Z0-9-\/"]+)/g);
+          const w = document.getText(document.getWordRangeAtPosition(position, regexSelector)).replaceAll('"','');
+          const completitions: CompletionItem[] = [];
+          for (const selector of this.extension.getDictionary().keys()) {
+            const item = new CompletionItem(selector, CompletionItemKind.Unit);
+
+            if (selector.match((`(${w.replaceAll('/', "\\/")})`))) {
+              if(w.endsWith("/")){
+                item.insertText = selector.substring(w.length, selector.length);
+              }
+              completitions.push(item);
+            }
+          }
+
+          return completitions;
+        },
+        resolveCompletionItem: (item) => {
+          switch (item.kind) {
+            case CompletionItemKind.Unit:
+              item.detail = "iconsauce";
+              const makdownContent = new MarkdownString(
+                `<h1><span style="color:#fff;background-color:#fff;">-<img src="${this.extension
+                  .getDictionary()
+                  .get(
+                    item.label.toString()
+                  )}" width="24px" height="24px"/>-</span></h1>`
+              );
+              makdownContent.supportHtml = true;
+              makdownContent.isTrusted = true;
+              item.documentation = makdownContent;
+
+              break;
+          }
+          return item;
+        },
       },
-      resolveCompletionItem: (item) => {
-        switch (item.kind) {
-          case CompletionItemKind.Unit:
-            item.detail = "iconsauce";
-            const makdownContent = new MarkdownString(`<h1><span style="color:#fff;background-color:#fff;">-<img src="${this.extension.getDictionary().get(item.label.toString())}" width="24px" height="24px"/>-</span></h1>`);
-            makdownContent.supportHtml = true;
-            makdownContent.isTrusted = true;
-            item.documentation = makdownContent;
-            break;
-        }
-        return item;
-      },
-    });
+      '"',
+      '/'
+    );
   }
 }
